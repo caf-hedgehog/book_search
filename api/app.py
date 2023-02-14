654677a840db5cd21d752dec367b5df269f6f2f1
caf-hedgehog <hcejynny@gmail.com>
2023-02-14 17:30:27 +0900

    create test data

--- api/app.py
+++ api/app.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    status,
+)
+from fastapi.responses import JSONResponse
 from bs4 import BeautifulSoup
+from elasticsearch import AsyncElasticsearch, Elasticsearch, helpers
+from elasticsearch.client import IndicesClient
+
+import asyncio
 import requests
 import pandas as pd
+import json
+import csv
 
 app = FastAPI()
+
+#Elasticsearchクライアント作成
+ES_URL = 'http://elasticsearch:9200'
+ES_USER = ''
+ES_PASS = ''
+es = AsyncElasticsearch(ES_URL, http_auth=(ES_USER, ES_PASS))
+indices_client = IndicesClient(es)
 
 #スレイピング先URLの指定
 BOOK_URL = "https://www.kinokuniya.co.jp/disp/CSfDispListPage_001.jsp?qs=true&ptk=01&q=湊かなえ" #今回はサンプルデータとして湊かなえの本を複数検索
@@ -62,4 +79,44 @@ def index():
     df = pd.io.json.json_normalize(books)
     df.to_csv('data/result.csv')
 
-    return "Done."
+    return JSONResponse(
+        content={"msg": "input data OK"}, status_code=status.HTTP_200_OK
+    )
+
+#Invoke-RestMethod -Uri "http://localhost:8002/create_test_data" -Method GET (windows power shell)
+#curl -X GET "http://localhost:8002/create_test_data"
+@app.get("/create_test_data", status_code=status.HTTP_200_OK)
+async def create_test_data():
+    saveSize = 10
+    
+    try:
+        with open('./data/result.csv', encoding='utf-8') as data:
+            print("csv read")
+            csvReader = csv.DictReader(data)
+            actions = []
+
+            for row in csvReader:
+                source = row
+                action = {
+                    "_index": "product-index",
+                    "_op_type": "index",
+                    "_id": row.get("title"),
+                    "_source": source
+                }
+                actions.append(action)
+                print(len(actions))
+                if len(actions) >= saveSize:
+                    await helpers.async_bulk(es, actions)
+                    del actions[0:len(actions)]
+
+            if len(actions) > 0:
+                print("Writing data to es")
+                await helpers.async_bulk(es, actions)
+                
+            return JSONResponse(
+                content={"msg": "Data created successfully!"}, status_code=status.HTTP_200_OK
+            )
+    except Exception as e:
+        # return JSONResponse(content=e, status_code=status.HTTP_400_BAD_REQUEST)
+        print(e)
+        return e
