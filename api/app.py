c7394ee80c076825c709d36ec863f70a1b3caa56
caf-hedgehog <hcejynny@gmail.com>
2023-02-20 22:20:38 +0900

    added createData and create index

--- api/app.py
+++ api/app.py
@@ -19,7 +19,8 @@ app = FastAPI()
 ES_URL = 'http://elasticsearch:9200'
 ES_USER = ''
 ES_PASS = ''
-es = AsyncElasticsearch(ES_URL, http_auth=(ES_USER, ES_PASS))
+# es = Elasticsearch(ES_URL, http_auth=(ES_USER, ES_PASS))
+es = AsyncElasticsearch(ES_URL)
 indices_client = IndicesClient(es)
 
 #スレイピング先URLの指定
@@ -28,7 +29,7 @@ BOOK_URL = "https://www.kinokuniya.co.jp/disp/CSfDispListPage_001.jsp?qs=true&pt
 #Invoke-RestMethod -Uri "http://localhost:8002" -Method GET (windows power shell)
 #curl -X GET "http://localhost:8002"
 @app.get("/")
-def index():
+async def index():
     return {"Hello" : "World!!!"}
 
 #Invoke-RestMethod -Uri "http://localhost:8002/dummy_data" -Method GET (windows power shell)
@@ -43,12 +44,13 @@ def index():
     html_data = BeautifulSoup(res.text, "html.parser")
     item_list = html_data.select('[class="list_area_wrap"] [class="heightLine-2"] a')
     
-
+    id = 0
     books = []
 
     for i in item_list:
         #本の情報を格納するdictを用意
         book_data = {
+            "id": id,
             "title": "",
             "writer": "",
             "description": ""
@@ -73,11 +75,11 @@ def index():
             book_data["description"] = "None"
         
         books.append(book_data)
+        id += 1
 
-    # print(books)
     pd.set_option('display.unicode.east_asian_width', True)
     df = pd.io.json.json_normalize(books)
-    df.to_csv('data/result.csv')
+    df.to_csv('data/result.csv', index=False)
 
     return JSONResponse(
         content={"msg": "input data OK"}, status_code=status.HTTP_200_OK
@@ -94,17 +96,15 @@ async def create_test_data():
             print("csv read")
             csvReader = csv.DictReader(data)
             actions = []
-
             for row in csvReader:
                 source = row
                 action = {
                     "_index": "product-index",
                     "_op_type": "index",
-                    "_id": row.get("title"),
+                    "_id": row.get("id"),
                     "_source": source
                 }
                 actions.append(action)
-                print(len(actions))
                 if len(actions) >= saveSize:
                     await helpers.async_bulk(es, actions)
                     del actions[0:len(actions)]
@@ -117,6 +117,93 @@ async def create_test_data():
                 content={"msg": "Data created successfully!"}, status_code=status.HTTP_200_OK
             )
     except Exception as e:
-        # return JSONResponse(content=e, status_code=status.HTTP_400_BAD_REQUEST)
-        print(e)
-        return e
+        return JSONResponse(content=to_json(e), status_code=status.HTTP_400_BAD_REQUEST)
+
+
+########## index ##################################################
+
+# curl -X POST -H "Content-Type: application/json" localhost:8002/indices/product-index
+@app.post("/indices/{index_name}", status_code=status.HTTP_200_OK)
+async def create_index(index_name):
+
+    #kuromoji settings
+    setting = {
+        "settings": {
+            "analysis": {
+                "tokenizer": {
+                    "kuromoji_search": {
+                        "type": "kuromoji_tokenizer",
+                        "mode": "search"
+                    }
+                },
+
+                "analyzer": {
+                    "my_kuromoji_analyzer": {
+                        "type": "custom",
+                        "tokenizer": "kuromoji_search",
+                        "char_filter": [
+                            "kuromoji_iteration_mark"
+                        ],
+                        "filter": [
+                            "kuromoji_part_of_speech",
+                            "kuromoji_number",
+                            "kuromoji_baseform",
+                            "ja_stop"
+                        ]
+                    }
+                },
+
+                "filter": {
+                    "kuromoji_part_of_speech": {
+                        "type": "kuromoji_part_of_speech"
+                    }
+                }
+            }
+        }
+    }
+    try:
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        res = await es.indices.create(index=index_name, body=setting)
+        return JSONResponse(content=to_json(res), status_code=status.HTTP_200_OK)
+    except Exception as e:
+        return JSONResponse(content=to_json(e), status_code=status.HTTP_400_BAD_REQUEST)
+
+# curl localhost:8002/indices/{index_name} -H "Content-Type: application/json"
+@app.get("/indices/{index_name}")
+async def get_index(index_name):
+    mapping = {
+        "mapping": {
+            "properties": {
+                "content": {
+                    "type": "text",
+                    "analyzer": "my_kuromoji_analyzer",
+                    "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "ignore_above": 8191
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    try:
+        res = await es.indices.get_mapping(index=index_name, body=mapping)
+        return JSONResponse(content=to_json(res), status_code=status.HTTP_200_OK)
+    except Exception as e:
+        return JSONResponse(content=to_json(e), status_code=status.HTTP_400_BAD_REQUEST)
+
+# curl -X DELETE localhost:8002/indices/product-index
+@app.delete("/indices/{index_name}")
+async def delete_index(index_name):
+    try:
+        res = await es.indices.delete(index=index_name)
+        return JSONResponse(content=to_json(res), status_code=status.HTTP_200_OK)
+    except Exception as e:
+        return JSONResponse(content=to_json(e), status_code=status.HTTP_400_BAD_REQUEST)
+
+def to_json(content):
+    return json.loads(json.dumps(content, default=str))
